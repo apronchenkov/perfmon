02b3845d07af7f4a377e52d470ec4f56785e4bfe
Alexander G. Pronchenkov <alexander@pronchenkov.name>
2013-02-18 01:57:32 +0400

    Working on the interface.

--- perfmon.h
+++ perfmon.h
@@ -1,31 +1,68 @@
 #pragma once
 
+/**
+ * Performance monitor library
+ *
+ * Usage:
+ *
+ *   You could use the PERFMON_FUNCTION_SCOPE macro for functions:
+ *
+ *     void function()
+ *     {
+ *         PERFMON_FUNCTION_SCOPE;
+ *         ... // function body
+ *     }
+ *
+ *   or the PERFMON_SCOPE for any scope
+ *
+ *     {
+ *         PERFMON_SCOPE("scope_name");
+ *         ... // scope body
+ *     }
+ *
+ *   WARNING! Most probably you would not like to use the PERFMON macros in
+ *   recursive scopes. You could, if you understand what you want; but values of
+ *   tick counter would be a bit crazy for a human being.
+ *
+ *   You could get current statistics in the following way:
+ *
+ *     for (const auto& counter : PERFMON_COUNTERS) {
+ *        std::cout << counter.name << ' ' << counter.calls << ' ' << counter.seconds() << std::endl;
+ *     }
+ */
+
 #include <atomic>
-#include <list>
-#include <utility>
 
 namespace perfmon {
 
 struct Counter {
+    /** Total number of calls */
     std::atomic_uint_fast64_t calls;
+
+    /** Total ticks have spent */
     std::atomic_uint_fast64_t ticks;
+
+    /** Name of the counter scope */
+    const char* name;
+
+    ///** Total seconds have spent */
+    double Seconds() const;
 };
-
-typedef std::list<Counter>::const_iterator Iterator;
-
-Counter& counter(const char* name);
-std::pair<Iterator, Iterator> counters();
 
 } // perfmon
 
+/** Get list of the available counters */
+#define PERFMON_COUNTERS
 
-#define PERFMON_COUNTERS                        (::perfmon::counters())
-#define PERFMON_COUNTER(name)                   ([] -> ::perfmon::Counter& { static auto& counter = ::perfmon::counter(name); return counter; }())
+/** Get a counter with specific name */
+#define PERFMON_COUNTER(counter_name)
 
+/** Create a monitor for current scope */
+#define PERFMON_SCOPE(counter_name)
 
-#define PERFMON_INTERNAL__CONCAT(x, y)          x ## y
-#define PERFMON_INTERNAL_CONCAT(x, y)           PERFMON_INTERNAL__CONCAT(x, y)
-#define PERFMON_INTERNAL_SCOPE_MONITOR_NAME     PERFMON_INTERNAL_CONCAT(perfmon_scope_monitor_, __COUNTER__)
+/** Create a monitor with current function name */
+#define PERFMON_FUNCTION_SCOPE    PERFMON_SCOPE(__FUNCTION__)
 
-#define PERFMON_SCOPE(name)                     const ::perfmon::Monitor PERFMON_INTERNAL_SCOPE_MONITOR_NAME (PERFMON_COUNTER(name))
-#define PERFMON_FUNCTION_SCOPE                  PERFMON_SCOPE (__FUNCTION__)
+#define PERFMON_INL_H
+#include "perfmon_inl.h"
+#undef PERFMON_INL_H
---
+++ perfmon_inl.h
@@ -0,0 +1,75 @@
+#ifndef PERFMON_INL_H
+#error "You should no use this file directly. Please include the 'perfmon.h'."
+#endif // PERFMON_INL_H
+
+#include "ticks.h"
+#include <list>
+
+namespace perfmon {
+namespace internal {
+
+class Monitor {
+public:
+    explicit Monitor(::perfmon::Counter& counter_ref)
+      : counter_ref_(counter_ref)
+      , start_tick_(ReadTickCounter())
+    { }
+
+    ~Monitor()
+    {
+        counter_ref_.ticks += TicksElapsed(start_tick_, ReadTickCounter());
+        ++counter_ref_.calls;
+    }
+
+private:
+    ::perfmon::Counter& counter_ref_;
+    const Tick start_tick_;
+};
+
+class Counters {
+public:
+    typedef std::list<Counter>::const_iterator const_iterator;
+
+    Counters(const_iterator begin, const_iterator end)
+      : begin_(begin)
+      , end_(end)
+    { }
+
+    const_iterator begin() const
+    {
+        return begin_;
+    }
+
+    const_iterator end() const
+    {
+        return end_;
+    }
+
+private:
+    const_iterator begin_;
+    const_iterator end_;
+};
+
+
+Counters counters();
+
+Counter& counter(const char* counter_name);
+
+} } // namespace perfmon::internal
+
+
+#undef PERFMON_COUNTERS
+#undef PERFMON_COUNTER
+#undef PERFMON_SCOPE
+
+#define PERFMON_COUNTERS               (::perfmon::internal::counters())
+#define PERFMON_COUNTER(counter_name)  ([]()->::perfmon::Counter& { static auto& counter_ref = ::perfmon::internal::counter(counter_name); return counter_ref; }())
+
+#ifdef NDEBUG
+#define PERFMON_SCOPE(counter_name)    do { } while(false)
+#else // NDEBUG
+#define PERFMON_INTERNAL__CONCAT(x, y) x ## y
+#define PERFMON_INTERNAL_CONCAT(x, y)  PERFMON_INTERNAL__CONCAT(x, y)
+#define PERFMON_INTERNAL_SCOPE_VAR     PERFMON_INTERNAL_CONCAT(perfmon_scope_monitor_, __COUNTER__)
+#define PERFMON_SCOPE(counter_name)    const ::perfmon::internal::Monitor PERFMON_INTERNAL_SCOPE_VAR (PERFMON_COUNTER(counter_name))
+#endif // NDEBUG
