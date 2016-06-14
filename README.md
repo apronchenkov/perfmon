3fdfc54326e4d4f27f116472b01545a1e727307d
Alexander G. Pronchenkov <alexander@pronchenkov.name>
2016-06-14 14:56:26 -0400

    Updated README.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-Performance monitor library
-===========================
+Performance counters library
+============================
 
-A simple performance monitor for C++11.
+Simple performance counters for C++11.
 
 
 Usage
@@ -28,14 +28,17 @@ WARNING! Most probably you would not like to use the PERFMON macros in
 recursive scopes. You could, if you understand what you want; but values of
 tick counter would be a bit crazy for a human being.
 
+
 You could get current statistics in the following way:
 
 ```cpp
 for (const auto& counter : PERFMON_COUNTERS()) {
-    std::cout << counter.Name() << ' ' << counter.Calls() << ' ' << counter.Seconds() << std::endl;
+    std::cout << counter.Name() << ' '
+              << counter.Calls() << ' '
+              << counter.Seconds() << std::endl;
 }
 ```
 
-Values of PERFMON_COUNTERS() can be influenced by non-atomicity
-of the integer arithmetic. I believe that this problem should be very rare.
-You can also call PERFMON_COUNTER() three times in row and use the median.
+NOTICE! Values of `PERFMON_COUNTERS()` can be imprecise because of
+non-atomicity of the integer arithmetic. If you need to get a better
+precision, you can call `PERFMON_COUNTERS()` multiple times and use the median.
--- include/perfmon.h
+++ include/perfmon.h
@@ -1,36 +1,41 @@
 /**
- * Performance monitor library
+ * Simple performance counters for C++11.
  *
- * Usage:
+ * Usage
+ * -----
  *
- *   You could use the PERFMON_SCOPE for a scope inside of a function:
+ * You could use the PERFMON_SCOPE for a scope inside of a function:
  *
- *     {
- *         PERFMON_SCOPE("counter_name");
- *         ... // scope body
- *     }
+ *  {
+ *    PERFMON_SCOPE("scope_name");
+ *    ... // scope body
+ *  }
  *
- *   or the PERFMON_STATEMENT for a statement:
+ * or the PERFMON_STATEMENT for a statement:
  *
- *     PERFMON_STATEMENT("counter_name") {
- *         ... // statement
- *     }
+ *  PERFMON_STATEMENT("counter_name") {
+ *    ... // statement
+ * }
  *
- *   WARNING! Most probably you would not like to use the PERFMON macros in
- *   recursive scopes. You could, if you understand what you want; but values of
- *   tick counter would be a bit crazy for a human being.
+ * WARNING! Most probably you would not like to use the PERFMON macros in
+ * recursive scopes. You could, if you understand what you want; but values of
+ * tick counter would be a bit crazy for a human being.
  *
- *   You could get current statistics in the following way:
  *
- *     for (const auto& counter : PERFMON_COUNTERS()) {
- *        std::cout << counter.Name() << ' ' << counter.Calls() << ' ' << counter.Seconds() << std::endl;
- *     }
+ * You could get current statistics in the following way:
  *
- *   Values of PERFMON_COUNTERS() can be influenced by non-atomicity
- *   of the integer arithmetic. I believe that this problem should be very rare.
- *   You can also call PERFMON_COUNTER() three times in row and use the median.
+ *   for (const auto& counter : PERFMON_COUNTERS()) {
+ *     std::cout << counter.Name() << ' '
+ *               << counter.Calls() << ' '
+ *               << counter.Seconds() << std::endl;
+ *   }
  *
+ * NOTICE! Values of `PERFMON_COUNTERS()` can be imprecise because of
+ * non-atomicity of the integer arithmetic. If you need to get a better
+ * precision, you can call `PERFMON_COUNTERS()` multiple times and use
+ * the median.
  */
+
 #pragma once
 
 #include <cstdint>
